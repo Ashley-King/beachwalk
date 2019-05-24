9fba2d936b1e1f76a322d13084fb4201da5078b0
Ashley King <ashleyot@gmail.com>
2019-05-24 13:12:31 -0400

    global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import GlobalStyles from "./components/globals/GlobalStyles";
 import Home from "./pages/Home";
 export default class App extends Component {
   render() {
     return (
       <>
+        <GlobalStyles />
+        <h1>Here's a heading</h1>
         <Home />
       </>
     );
---
+++ src/styles.js
@@ -0,0 +1,19 @@
+export const setColor = {
+  bigFootFeet: "#ee964b",
+  stilDeGrainYellow: "#f4d35e",
+  lightSeaGreen: "#28afb0",
+  blueSapphire: "#19647e",
+
+  champagnePink: "#F9DAD0",
+  paleCopper: "#D5896F",
+  primaryColor: "#DAB785",
+  mainWhite: "#fff",
+  mainBlack: "#1f271b",
+  mainGrey: "#ececec",
+  lightGrey: "f7f7f7"
+};
+
+export const setFont = {
+  main: "font-family: 'Lato', sans-serif;",
+  slanted: "font-family: 'Courgette', cursive;"
+};
