ca00356e800e593d5563b0d00081c1b752aba439
Ashley King <ashleyot@gmail.com>
2019-05-24 12:23:22 -0400

    created basic file structure and added new favicon

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../components/home/Header";
+const Home = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
