bcf0bb3b8864e2e82d8ba7d2b27e7b6f32d8576c
Ashley King <ashleyot@gmail.com>
2019-05-28 15:13:15 -0400

    Room and Rooms components

---
+++ src/components/home/Room.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SmallBtn } from "../globals/Buttons";
+
+const Room = () => {
+  return (
+    <div>
+      <SmallBtn>hello from single room</SmallBtn>
+    </div>
+  );
+};
+
+export default Room;
